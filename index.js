25fb0cfb6d1cb6b3c7d0f3829f39201479db0cba
Victor <victortran96@gmail.com>
2020-03-25 14:37:07 -0500

    Update index.js

--- index.js
+++ index.js
@@ -47,9 +47,11 @@ class Game extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      history: [{
-        squares: Array(9).fill(null)
-      }],
+      history: [
+        {
+          squares: Array(9).fill(null)
+        }
+      ],
       stepNumber: 0,
       xIsNext: true
     };
@@ -64,9 +66,12 @@ class Game extends React.Component {
     }
     squares[i] = this.state.xIsNext ? 'X' : 'O';
     this.setState({
-      history: history.concat([{
+      history: history.concat([
+        {
         squares: squares
-      }]),
+        }
+      ]),
+      stepNumber: history.length,
       xIsNext: !this.state.xIsNext,
     });
   }
